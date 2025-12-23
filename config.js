b88cd82a2599132374f1b2fae618e557a0cb8104
amircrypto210-create <amircrypto210@gmail.com>
2025-12-23 18:52:44 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// فایل: config.js
+// این فایل را جداگانه ایجاد کنید و در index.html و show-location.html لود کنید
+
+const GITHUB_CONFIG = {
+    username: 'amircrypto210-create', // جایگزین کنید
+    repo: 'amirbordbarweb', // همان نام ریپازیتوری
+    token: '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', // توکنی که گرفتید
+    dataFile: 'locations.json'
+};
