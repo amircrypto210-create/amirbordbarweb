16afa2f7ed1361826bb6e3490199c19706b588f0
amircrypto210-create <amircrypto210@gmail.com>
2025-12-25 22:06:56 +0330

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// فایل: config.js
-// این فایل را جداگانه ایجاد کنید و در index.html و show-location.html لود کنید
-
-const GITHUB_CONFIG = {
-    username: 'amircrypto210-create', // جایگزین کنید
-    repo: 'amirbordbarweb', // همان نام ریپازیتوری
-    token: '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', // توکنی که گرفتید
-    dataFile: 'locations.json'
-};
